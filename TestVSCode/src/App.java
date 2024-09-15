791e3b31594254abe192435900eab2a623cdae1d
Ray Klump <klumpra@lewisu.edu>
2024-09-15 12:16:13 -0500

    added a line of output to App.java fro TestVSCode project

--- TestVSCode/src/App.java
+++ TestVSCode/src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("This is a test of VS Code's git tools.");
+        System.out.println("Then I imported it into Eclipse using File >> Open Projects from File System");
     }
 }
