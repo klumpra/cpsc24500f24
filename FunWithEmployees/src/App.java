3150d6f9f235719dde86a27abf2cc29f9a11fe84
klumpra <klumpra@lewisu.edu>
2024-10-25 10:58:12 -0500

    Commit before doing MVC on 10/25/2024

---
+++ FunWithEmployees/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Employee emp1 = new Employee();
+        Employee emp2 = new Employee("Ray","Klump",54.2,25.75);
+        System.out.println(emp1);
+        System.out.println(emp2);
+    }
+}
